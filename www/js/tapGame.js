2daad85f65484cf4f570f02aabd8f5dbcc3d2a84
k21rs130 <k21rs130@st.kyusan-u.ac.jp>
2022-12-15 09:09:59 +0900

    問題2 ランキングの表示

--- www/js/tapGame.js
+++ www/js/tapGame.js
@@ -55,8 +55,23 @@ function saveScore(name, score) {
             console.log("保存に失敗しました。エラー:" + error);
         });
 
-
-
+    // **********【問題２】ランキングを表示しよう！**********
+    // 保存先クラスを作成
+    var highScore = ncmb.DataStore("GameScore");
+    // scoreの降順でデータ5件を取得するように設定する
+    highScore.order("score", true)
+        .limit(5)
+        .fetchAll()
+        .then(function (results) {
+            // 検索に成功した場合の処理
+            console.log("検索に成功しました。");
+            // テーブルにデータをセット
+            setData(results);
+        })
+        .catch(function (error) {
+            // 検索に失敗した場合の処理
+            console.log("検索に失敗しました。エラー:" + error);
+        });
 
 
 
